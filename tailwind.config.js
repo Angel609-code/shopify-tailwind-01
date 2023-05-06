ad125be051137bac5bbf390ecfe77f5cbae1424a
Jose Guerrero <Angelguerrerojose609@gmail.com>
2023-05-06 13:13:49 -0600

    Implemented improvements to the homepage and 404 template

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '2.953rem',
+      '4xl': '3.441rem',
+      '5xl': '5.052rem',
+    }
   },
   plugins: [],
 }
